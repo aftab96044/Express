9a98772121e9ae9a3a63b61c4287da31fbd2ac7c
aftab96044 <aftab.neymar.10@gmail.com>
2026-06-15 06:39:24 +0530

    json

---
+++ json.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import userData from './users.json' with {type: 'json'}
+const app = express()
+
+app.get("/",(req, res)=>{
+    console.log(userData);
+    
+    res.send((userData))
+})
+app.get("/user/:id",(req, res)=>{
+    const id = req.params.id
+    console.log(id);
+    let filteredData = userData.filter((user)=>user.id==id)
+    res.send(filteredData)
+})
+app.get("/username/:name",(req, res)=>{
+    const name = req.params.name
+    console.log(name);
+    let filteredData = userData.filter((user)=>user.name==name)
+    res.send(filteredData)
+})
+app.listen(1100)
+
+
+
+
+
+
+
+
+
+
+
