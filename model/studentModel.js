6ba3a3135119fb900f80a263c718f55fe2f9dee0
aftab96044 <aftab.neymar.10@gmail.com>
2026-06-18 21:21:27 +0530

    stude

---
+++ model/studentModel.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose'
+import studentSchema from '../schema/studentSchema.js'
+
+const studentModel = mongoose.model("students", studentSchema)
+
+export default studentModel
+
+
