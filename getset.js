84035bd34d03b608017487776e0f549759b669ab
aftab96044 <aftab.neymar.10@gmail.com>
2026-06-18 21:20:20 +0530

    gs

---
+++ getset.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import session from 'express-session'
+const app = express()
+
+app.set("view engine", "ejs")
+
+app.use(session({
+    secret:'apple'
+
+}))
+
+app.use(express.urlencoded({extended:true}))
+app.get("/login",(req, res)=>{
+    res.render('Login')
+})
+
+app.post("/profile",(req, res)=>{
+    req.session.data= req.body;
+    console.log(req.session.data);
+    
+    res.render('profile')
+})
+
+app.get("/",(req, res)=>{
+    const data = req.session.data
+    console.log("data",data);
+    
+    res.render('homes',{data})
+})
+
+app.listen(1100)
